6dfb6d1d5fcb9867d3bd7d10b6b0e89211e02082
Julianne David <juliannedavid@gmail.com>
2020-04-20 18:36:27 -0700

    begins refactor, adds TODO list

--- precision_recall_jd_refactor.py
+++ precision_recall_jd_refactor.py
@@ -19,6 +19,12 @@ _LOG_MODE = 'INFO'
 _ACCEPTABLE_MOTIFS = ['GTAG', 'GCAG', 'ATAC']
 
 
+#TODO: first run to collect mutual sample_id list?  Or - parse as we go?
+#TODO: switch from "lite" col names to full
+#TODO: differential loading of v2/v3?
+#TODO: update find_coverages to work with .apply instead of modifying input df
+#TODO: improve cycling through samples - leverage pandas or parse as we go?
+
 #user specified inputs needed for precision & recall calculations (prc)
 sample_ids = ['18538','28168', '45103', '46189', '7613'] 
 #motif to filter junctions by
@@ -34,7 +40,7 @@ dataframe_col_names = ['chromosome', 'start', 'end', 'length', 'strand',
 col_names_for_prc = ['chromosome', 'start', 'end', 'strand', sample_num]
 
 
-def find_coverages(sample_ids,cov_col_name, dataframe):
+def find_coverages(sample_ids, cov_col_name, dataframe):
     #processes input data frame, calculates scaled coverage values 
     #for each sample, adds column with sample id and scaled coverages
     #for each junction to the input data frame 
@@ -46,19 +52,19 @@ def find_coverages(sample_ids,cov_col_name, dataframe):
         cov_values = []
         sample_id_string = ',' + sample_id + ':'                    
         for cov_string in cov_strings: 
-            #print(sample_string)\n',
+            #logging.info(sample_string)\n',
             if sample_id_string in cov_string: 
                 #isolates sample of interest from other samples found in junction
                 first_split = cov_string.split(sample_id_string) 
                 #isolates cov value for sample of interest
                 second_split = first_split[1].split(',')
                 cov_value = int(second_split[0]) 
-            else: 
-              
-                cov_value = 0 
+            else:
+                cov_value = 0
+
             cov_values.append(cov_value)
         total_cov = sum(cov_values)
-        #print(total_cov)
+        #logging.info(total_cov)
         dataframe[sample_id] = [cov_value/total_cov for cov_value in cov_values]
         cum_cov = cum_cov + total_cov
         
@@ -83,6 +89,54 @@ def load_data(jx_file):
     jx_df.loc[:, sample_num] *= avg_total_cov_srav2
     jx_df['motif'] = jx_df['5prime_motif']  + jx_df['3prime_motif']
     return jx_df
+
+
+def filter_and_compare_jxs(srav2_data, srav3_data):
+    coverage_thresholds = [(0, 0), (5, 5), (10, 10)]
+    logging.info(coverage_thresholds)
+    for cov_thresh in coverage_thresholds:
+        # creating new data frames for ground truth & test that are filtered by
+        # user-specified thresholds for the motif and scaled coverage values
+        srav2_data_filtered = srav2_data[
+            (srav2_data['motif'] == filter_motif)
+            & (srav2_data[sample_num] != 0)
+            & (srav2_data[sample_num] >= cov_thresh[0])
+            ]
+
+        srav3_data_filtered = srav3_data[
+            (srav3_data['motif'] == filter_motif)
+            & (srav3_data[sample_num] != 0)
+            & (srav3_data[sample_num] >= cov_thresh[1])
+            ]
+
+        # calculating precision and recall
+
+        # number of samples in ground truth
+        srav2_num_juncs = srav2_data_filtered.shape[0]
+
+        # number of samples in test
+        srav3_num_juncs = srav3_data_filtered.shape[0]
+        merged_data = pd.merge(srav2_data_filtered[col_names_for_prc],
+                               srav3_data_filtered[col_names_for_prc],
+                               on=['chromosome', 'start', 'end', 'strand'],
+                               how='inner')
+
+        # number of samples shared by ground truth and test
+        num_shared_juncs = merged_data.shape[0]
+        logging.info("Coverage threshold: " + str(cov_thresh))
+        if num_shared_juncs != 0:
+            precision = num_shared_juncs / srav3_num_juncs
+            recall = num_shared_juncs / srav2_num_juncs
+            logging.info("Precision:" + str(precision))
+            logging.info("Recall: " + str(recall))
+        else:
+            logging.info("No shared junctions.")
+
+    logging.info("srav2 filtered data")
+    logging.info(srav2_data_filtered[col_names_for_prc])
+    logging.info("srav3 filtered data")
+    logging.info(srav3_data_filtered[col_names_for_prc])
+    return
 
 
 def main():
@@ -117,53 +171,11 @@ def main():
     srav2_data = load_data(recount2)
     srav3_data = load_data(recount3)
 
-   # print ("before filtering\n")
-   # print(srav2_data.shape[0])
-   # print(srav3_data.shape[0])
+    # logging.info ("before filtering\n")
+    # logging.info(srav2_data.shape[0])
+    # logging.info(srav3_data.shape[0])
 
-    coverage_thresholds = [(0,0), (5,5), (10,10)]
-    logging.info(coverage_thresholds)
-    for cov_thresh in coverage_thresholds:
-        # creating new data frames for ground truth & test that are filtered by
-        # user-specified thresholds for the motif and scaled coverage values
-        srav2_data_filtered = srav2_data[
-            (srav2_data['motif'] == filter_motif)
-            & (srav2_data[sample_num] != 0)
-            & (srav2_data[sample_num] >= cov_thresh[0])
-        ]
-       
-        srav3_data_filtered = srav3_data[
-            (srav3_data['motif'] == filter_motif)
-            & (srav3_data[sample_num] != 0)
-            & (srav3_data[sample_num] >= cov_thresh[1])
-        ]
-
-        #calculating precision and recall 
-
-        #number of samples in ground truth
-        srav2_num_juncs = srav2_data_filtered.shape[0]
-        
-        #number of samples in test 
-        srav3_num_juncs = srav3_data_filtered.shape[0] 
-        merged_data = pd.merge(srav2_data_filtered[col_names_for_prc], 
-                       srav3_data_filtered[col_names_for_prc], 
-                       on=['chromosome', 'start', 'end', 'strand'],
-                       how = 'inner')
-
-        #number of samples shared by ground truth and test 
-        num_shared_juncs = merged_data.shape[0]
-        print ("Coverage threshold: " + str(cov_thresh))
-        if num_shared_juncs != 0: 
-            precision = num_shared_juncs/srav3_num_juncs
-            recall = num_shared_juncs/srav2_num_juncs
-            print("Precision:" + str(precision))
-            print("Recall: " + str(recall))
-        else: 
-            print ("No shared junctions.")
-    print("srav2 filtered data")
-    print(srav2_data_filtered[col_names_for_prc])
-    print("srav3 filtered data")
-    print(srav3_data_filtered[col_names_for_prc])
+    filter_and_compare_jxs(srav2_data, srav3_data)
 
 
 if __name__ == '__main__':
