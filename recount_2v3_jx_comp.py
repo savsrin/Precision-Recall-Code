5ff40a4e873ed828f50f4851ac8deadfbb2662ab
davidju <juliannedavid@gmail.com>
2020-05-11 14:45:06 -0400

    fixes bugs, removes plotting

--- recount_2v3_jx_comp.py
+++ recount_2v3_jx_comp.py
@@ -13,16 +13,12 @@ from datetime import datetime
 import gzip
 import json
 import logging
-import matplotlib.patches as mpatches
-import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
 import os
 import pandas as pd
-import seaborn as sns
 import sys
+import time
 
 csv.field_size_limit(sys.maxsize)
-sns.set(color_codes=True)
 
 _LOG_MODE = 'INFO'
 _CANON_MOTIFS = ['GTAG', 'GCAG', 'ATAC']
@@ -40,167 +36,6 @@ _EVENTS = 'events'
 
 class JXIndexError(Exception):
     pass
-
-
-def grouped_boxplots(data_dict, plot_dict, fig_file, fig_size=(7.0, 4.0),
-                     logscale=False, y_label= 'recall', percent=True,
-                     right_lim_shift=2, x_label='splice site motif'):
-    """
-
-    :param data_dict:
-    :param plot_dict:
-    :param fig_file:
-    :param fig_size:
-    :param logscale:
-    :param y_label:
-    :param percent:
-    :param right_lim_shift:
-    :param x_label:
-    :return:
-    """
-    plt.rcParams.update({'figure.autolayout': True})
-    plt.rcParams['figure.figsize'] = fig_size
-    sns.set_context("paper")
-    sns.set_style("whitegrid")
-    sns.set(style="ticks")
-
-    f, ax = plt.subplots(nrows=1, ncols=1)
-    plt.sca(ax)
-    light_cols = plot_dict['light colors']
-    dark_cols = plot_dict['dark colors']
-    num_boxes = len(light_cols)
-    adjust_val = num_boxes + 1
-
-    bp_pos = list(range(1, num_boxes + 1))
-
-    label_locs = []
-    labels = [str(key) for key in data_dict]
-    curr_label_loc = sum(bp_pos) / len(bp_pos)
-
-    for abbr, values in data_dict.items():
-        labels.append(abbr)
-        data = values['data']
-
-        boxes = plt.boxplot(
-            data, positions=bp_pos, widths=0.6, patch_artist=True
-        )
-        box_elements = zip(boxes['fliers'], boxes['medians'], boxes['boxes'])
-        for i, (fly, med, box) in enumerate(box_elements):
-            plt.setp(
-                fly, color=light_cols[i], markerfacecolor=light_cols[i],
-                marker='.', markersize=4.0, linestyle='none', linewidth=0.15,
-                markeredgecolor=dark_cols[i]
-            )
-            plt.setp(
-                med, color=dark_cols[i], linewidth=1.5
-            )
-            plt.setp(box, facecolor='white')
-
-        label_locs.append(curr_label_loc)
-
-        bp_pos = [x + adjust_val for x in bp_pos]
-        curr_label_loc += adjust_val
-
-    ax.set_xticklabels(labels)
-    ax.set_xticks(label_locs)
-    ax.xaxis.grid(False)
-    ax.yaxis.grid(True)
-    ax.set_xlim(left=0, right=curr_label_loc-right_lim_shift)
-    ax.spines['bottom'].set_color('black')
-    ax.spines['top'].set_color('black')
-    ax.spines['right'].set_color('black')
-    ax.spines['left'].set_color('black')
-
-    if logscale:
-        plt.yscale('log')
-
-    plt.ylabel(y_label, fontsize=7)
-    plt.xlabel(x_label, fontsize=7)
-    plt.setp(
-        ax.xaxis.get_majorticklabels(), rotation=90, fontsize=5, color='black'
-    )
-    plt.setp(
-        ax.yaxis.get_majorticklabels(), fontsize=7, color='black'
-    )
-
-    legend_list = []
-    for p_col, p_lab in zip(plot_dict['dark colors'], plot_dict['row labels']):
-        legend_list.append(mpatches.Patch(color=p_col, label=p_lab))
-
-    plt.legend(handles=legend_list)
-
-    if percent:
-        ax.yaxis.set_major_formatter(
-            ticker.FuncFormatter(
-                lambda y, _: '{}%'.format('{:,g}'.format(100 * y))
-            )
-        )
-    else:
-        ax.yaxis.set_major_formatter(
-            ticker.FuncFormatter(lambda y, _: '{:,g}'.format(y))
-        )
-    plt.xticks([])
-    fig = plt.gcf()
-    fig.savefig(fig_file, bbox_inches='tight', pad_inches=.1)
-    return
-
-
-def prepare_boxplot_data(sample_jx_dict, outpath, now, cohort):
-    plot_info_dict = {
-        'light colors': ['#29A1E8', '#85C9F2', '#E8321F', '#F2867B'],
-        'dark colors':  ['#29A1E8', '#85C9F2', '#E8321F', '#F2867B'],
-        'row labels': [
-            'v3 junction recall: v2 ground truth',
-            'v3 detection event recall; v2 ground truth',
-            'v2 junction recall; v3 ground truth',
-            'v2 detection event recall; v2 ground truth'
-        ]
-    }
-
-    # Begin processing of all jxs/all motifs
-    plot_data_dict = {mot: {'data': [[], [], [], []]} for mot in _CANON_MOTIFS}
-    plot_data_dict['all motifs'] = {'data': [[], [], [], []]}
-
-    # Process each canonical motif
-    for motif in _CANON_MOTIFS + [_OTHER_MOT]:
-        for sample, data in sample_jx_dict.items():
-            v2_jxs = data[_JXS][_V2_JX][motif]
-            v3_jxs = data[_JXS][_V3_JX][motif]
-            mutual_jxs = data[_JXS][_MU_JX][motif]
-            v2_events = data[_EVENTS][_V2_JX][motif]
-            v3_events = data[_EVENTS][_V3_JX][motif]
-            mutual_events = data[_EVENTS][_MU_JX][motif]
-
-            jx_recall3 = mutual_jxs / (mutual_jxs + v3_jxs)
-            jx_recall2 = mutual_jxs / (mutual_jxs + v2_jxs)
-            event_recall3 = mutual_events / (mutual_events + v3_events)
-            event_recall2 = mutual_events / (mutual_events + v2_events)
-
-            plot_data_dict['all motifs']['data'][0].append(jx_recall2)
-            plot_data_dict['all motifs']['data'][1].append(event_recall2)
-            plot_data_dict['all motifs']['data'][2].append(jx_recall3)
-            plot_data_dict['all motifs']['data'][3].append(event_recall3)
-
-            if motif != _OTHER_MOT:
-                plot_data_dict[motif]['data'][0].append(jx_recall2)
-                plot_data_dict[motif]['data'][1].append(event_recall2)
-                plot_data_dict[motif]['data'][2].append(jx_recall3)
-                plot_data_dict[motif]['data'][3].append(event_recall3)
-
-    fig_file = os.path.join(outpath, '{}_allmotifs_{}.pdf'.format(cohort, now))
-    data_file = os.path.join(
-        outpath, '{}_allmotifs_plotinfo_{}.json'.format(cohort, now)
-    )
-    with open(data_file, 'w') as output:
-        dump_list = [
-            plot_data_dict, plot_info_dict, fig_file
-        ]
-        json.dump(dump_list, output)
-
-    grouped_boxplots(
-        plot_data_dict, plot_info_dict, fig_file
-    )
-    return
 
 
 def map_recount_ids_2to3(recount2_sample_ids, recount3_id_map):
@@ -266,7 +101,7 @@ def recount_firstpass(jx_file, recount_2to3_map=None):
     return sample_set
 
 
-def coordinates_from_jx_line(jx_line, coord_positions):
+def coords_from_jx_line(jx_line, coord_positions):
     """
 
     :param jx_line:
@@ -307,6 +142,11 @@ def add_mutual_jx_to_samp_dict(samp_dict, line2, line3, mutual_samples,
             temp_list.pop(0)
             samp_cov_3_list.extend(temp_list)
 
+    v3_samp_set = set()
+    for samp_info in samp_cov_3_list:
+        samp, _ = samp_info.split(':')
+        v3_samp_set.add(samp)
+
     if strand == '-':
         motif = motif.translate(_COMPLEMENT)[::-1]
 
@@ -317,7 +157,7 @@ def add_mutual_jx_to_samp_dict(samp_dict, line2, line3, mutual_samples,
             samp = recount2_id_map.get(samp, None)
             if samp not in mutual_samples:
                 continue
-            if samp in samp_cov_3_list:
+            if samp in v3_samp_set:
                 joint_samps[samp] = {'v2': int(cov)}
             else:
                 samp_dict[samp][_JXS][_V2_JX][motif] += 1
@@ -330,7 +170,7 @@ def add_mutual_jx_to_samp_dict(samp_dict, line2, line3, mutual_samples,
             samp = recount2_id_map.get(samp, None)
             if samp not in mutual_samples:
                 continue
-            if samp in samp_cov_3_list:
+            if samp in v3_samp_set:
                 joint_samps[samp] = {'v2': int(cov)}
             else:
                 samp_dict[samp][_JXS][_V2_JX][motif] += 1
@@ -360,7 +200,6 @@ def add_mutual_jx_to_samp_dict(samp_dict, line2, line3, mutual_samples,
 
         samp_dict[samp][_JXS][_MU_JX][motif] += 1
         samp_dict[samp][_EVENTS][_MU_JX][motif] += mutual_events
-
     return samp_dict
 
 
@@ -436,7 +275,7 @@ def add_recount_jx_to_samp_dict(samp_dict, jx_line, v2v3, motif, mutual_samps):
 
 
 def collect_jx_covs(v2_jxs, v3_jxs, mutual_samples, recount2_id_map,
-                    cohort_flag):
+                    cohort_flag, out_path):
     """
 
     :param v2_jxs:
@@ -447,11 +286,14 @@ def collect_jx_covs(v2_jxs, v3_jxs, mutual_samples, recount2_id_map,
     :param recount2_id_map:
     :return:
     """
+    v3_coord_locs = (1, 2, 3, 5)
     if cohort_flag == _SRA:
-        coord_locs = (0, 1, 2, 3)
+        v2_coord_locs = (0, 1, 2, 3)
     else:
-        coord_locs = (1, 2, 3, 5)
-
+        v2_coord_locs = (1, 2, 3, 5)
+    ticker = 0
+    t_int = time.time()
+    start_time = t_int
     samp_dict = {}
     for samp in mutual_samples:
         samp_dict[samp] = {
@@ -472,9 +314,24 @@ def collect_jx_covs(v2_jxs, v3_jxs, mutual_samples, recount2_id_map,
         file3 = csv.reader(v3file, delimiter='\t')
         v2_line = next(file2)
         v3_line = next(file3)
-        v2_coords = coordinates_from_jx_line(v2_line, coord_locs)
-        v3_coords = coordinates_from_jx_line(v3_line, coord_locs)
+        v2_coords = coords_from_jx_line(v2_line, v2_coord_locs)
+        v3_coords = coords_from_jx_line(v3_line, v3_coord_locs)
         while v2_line or v3_line:
+            ticker += 1
+            if (ticker % 10000000) == 0:
+                t_curr = time.time()
+                logging.info(
+                    '{}th jx, intermediate time is {}'
+                    ''.format(ticker, t_curr - t_int)
+                )
+                logging.info('total time is {}'.format(t_curr - start_time))
+                t_int = time.time() 
+                int_file = os.path.join(
+                    out_path, '{}_int_dict_{}.json'.format(cohort_flag, ticker)
+                )
+                with open(int_file, 'w') as output:
+                    json.dump(samp_dict, output)
+
             # Finish off v3 or v2 file after the other ends:
             if not v2_line:
                 st3 = v3_coords[3]
@@ -482,24 +339,26 @@ def collect_jx_covs(v2_jxs, v3_jxs, mutual_samples, recount2_id_map,
                     samp_dict, v3_line, st3, mutual_samples
                 )
                 v3_line = next(file3, None)
-                v3_coords = coordinates_from_jx_line(v3_line, coord_locs)
+                v3_coords = coords_from_jx_line(v3_line, v3_coord_locs)
                 continue
 
             if not v3_line:
                 samp_dict = add_v2_jx_to_samp_dict(
-                    samp_dict, v2_line, mutual_samples, recount2_id_map
+                    samp_dict, v2_line, mutual_samples, recount2_id_map, 
+                    cohort_flag
                 )
                 v2_line = next(file2, None)
-                v2_coords = coordinates_from_jx_line(v2_line, coord_locs)
+                v2_coords = coords_from_jx_line(v2_line, v2_coord_locs)
                 continue 
 
             # Main comparison: running through lines in both files
             if v2_coords < v3_coords:
                 samp_dict = add_v2_jx_to_samp_dict(
-                    samp_dict, v2_line, mutual_samples, recount2_id_map
+                    samp_dict, v2_line, mutual_samples, recount2_id_map,
+                    cohort_flag
                 )
                 v2_line = next(file2, None)
-                v2_coords = coordinates_from_jx_line(v2_line, coord_locs)
+                v2_coords = coords_from_jx_line(v2_line, v2_coord_locs)
                 continue
             elif v2_coords > v3_coords:
                 st3 = v3_coords[3]
@@ -507,7 +366,7 @@ def collect_jx_covs(v2_jxs, v3_jxs, mutual_samples, recount2_id_map,
                     samp_dict, v3_line, st3, mutual_samples
                 )
                 v3_line = next(file3, None)
-                v3_coords = coordinates_from_jx_line(v3_line, coord_locs)
+                v3_coords = coords_from_jx_line(v3_line, v3_coord_locs)
                 continue
             else:
                 # add jx to mutual
@@ -517,21 +376,21 @@ def collect_jx_covs(v2_jxs, v3_jxs, mutual_samples, recount2_id_map,
                 while v3_coords == v3_init_coords:
                     v3_line_list.append(v3_line)
                     v3_line = next(file3, None)
-                    v3_coords = coordinates_from_jx_line(v3_line, coord_locs)
+                    v3_coords = coords_from_jx_line(v3_line, v3_coord_locs)
 
                 if len(v3_line_list) == 1:
                     samp_dict = add_mutual_jx_to_samp_dict(
                         samp_dict, v2_line, v3_init_line, mutual_samples,
-                        recount2_id_map
+                        recount2_id_map, cohort_flag
                     )
                 else:
                     samp_dict = add_mutual_jx_to_samp_dict(
                         samp_dict, v2_line, v3_line_list, mutual_samples,
-                        recount2_id_map
+                        recount2_id_map, cohort_flag
                     )
 
                 v2_line = next(file2, None)
-                v2_coords = coordinates_from_jx_line(v2_line, coord_locs)
+                v2_coords = coords_from_jx_line(v2_line, v2_coord_locs)
                 continue
 
     return samp_dict
@@ -566,7 +425,7 @@ def execute_jx_comp(v2_file, v3_file, recount2_id_map, out_path, now, flag,
         '{}: {} mutual samples'.format(flag, len(mutual_samples))
     )    
     sample_jx_dict = collect_jx_covs(
-        v2_file, v3_file, mutual_samples, recount2_id_map, flag
+        v2_file, v3_file, mutual_samples, recount2_id_map, flag, out_path
     )
     data_file = os.path.join(
         out_path, '{}_sample_jx_dict_{}.json'.format(flag, now)
@@ -574,7 +433,7 @@ def execute_jx_comp(v2_file, v3_file, recount2_id_map, out_path, now, flag,
     with open(data_file, 'w') as output:
         json.dump(sample_jx_dict, output)
 
-    prepare_boxplot_data(sample_jx_dict, out_path, now, cohort=flag)
+    # prepare_boxplot_data(sample_jx_dict, out_path, now, cohort=flag)
     return
 
 
@@ -632,6 +491,10 @@ if __name__ == '__main__':
         '--gtex-mutual-samples',
         help='Provide a json file of GTEx samples in recount2 and recount3.'
     )
+    parser.add_argument(
+        '--sra-mutual-samples',
+        help='Provide a json file of SRA samples in recoun3 and recount3.'
+    )
 
     args = parser.parse_args()
     out_path = args.output_path
@@ -646,6 +509,7 @@ if __name__ == '__main__':
     tcga_ids3 = args.tcga_phenotypes_v3
     sra_ids3 = args.sra_phenotypes_v3
     gtex_both = args.gtex_mutual_samples
+    sra_both = args.sra_mutual_samples
 
     now = datetime.now().strftime('%m-%d-%Y_%H.%M.%S')
     log_file = os.path.join(
@@ -658,9 +522,11 @@ if __name__ == '__main__':
     recount2_id_map = map_recount_ids_2to3(ids_v2, recount3_id_map)
     
     # compare v2 to v3 jxs for three cohorts
+    # execute_jx_comp(
+    #     gtex_v2, gtex_v3, recount2_id_map, out_path, now, _GTEX, gtex_both
+    # )    
+    # execute_jx_comp(tcga_v2, tcga_v3, recount2_id_map, out_path, now, _TCGA)
     execute_jx_comp(
-        gtex_v2, gtex_v3, recount2_id_map, out_path, now, _GTEX, gtex_both
-    )    
-    execute_jx_comp(tcga_v2, tcga_v3, recount2_id_map, out_path, now, _TCGA)
-    execute_jx_comp(sra_v2, sra_v3, recount2_id_map, out_path, now, _SRA)
+        sra_v2, sra_v3, recount2_id_map, out_path, now, _SRA, sra_both
+    )
 
